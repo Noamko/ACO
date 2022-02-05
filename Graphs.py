0771329e5d766622a9528fea0f320af30233618c
Noam Koren <noamko25@gmail.com>
2022-02-05 14:31:50 +0200

    added exeptions for Graph initialization

--- Graphs.py
+++ Graphs.py
@@ -1,4 +1,4 @@
-import random 
+import random
 
 class Graph:
     class Node:
@@ -13,24 +13,33 @@ class Graph:
             return str(self.id)
 
         def getNeighbors(self) -> list:
-            return self.neighbors.copy()
+            return self.neighbors
+
+        def neighborsCount(self):
+            return len(self.neighbors)
 
     class Edge:
         def __init__(self, a, b, cost = 0) -> None:
+            if not isinstance(a, Graph.Node) or not isinstance(b, Graph.Node):
+                raise Exception("error: Can't initiate Edge witn none Graph.Node")
             self.a = a
             self.b = b
             self.cost = cost
+            self.a.neighbors.append(self.b)
+            self.b.neighbors.append(self.a)
 
         def __eq__(self, __o: object) -> bool:
             return (__o.a == self.a and __o.b == self.b) or (__o.b == self.a and __o.a == self.b)
 
         def __str__(self) -> str:
             return f"{self.a} ---{self.cost}--- {self.b}"
-        
-    def __init__(self, edges = [], nodes = []) -> None:
+    
+    def __init__(self, nodes = [], edges = []) -> None:
         self.edges = edges
         self.nodes = nodes
-        self.type = type
+        self.nodeMap = {}
+        for n in self.nodes:
+            self.nodeMap[n.id] = n
 
     def __str__(self) -> str:
         a = ""
@@ -38,23 +47,20 @@ class Graph:
             a += str(e) + "\n"
         return str(a)
 
-    def generate(self, size = 10, mincost = 1, maxcost = 100):
-        for i in range(size):
-            self.nodes.append(self.Node(i))
-
-        for n in self.nodes:
-            e = self.Edge(n, self.nodes[random.randint(0, size -1)],random.randint(mincost, maxcost))
-            if e.a != e.b and e not in self.edges:
-                e.a.neighbors.append(e.b)
-                e.b.neighbors.append(e.a)
-                self.edges.append(e)
-            
+    def getNodeById(self, id):
+        return self.nodeMap[id]
+    
+    def nodeCount(self):
+        return len(self.nodes)
 
 class DirectedGraph(Graph):
-    class DirectedEdge(Graph.Edge):
+    class Edge(Graph.Edge):
         def __init__(self, a, b, cost) -> None:
             super().__init__(a, b, cost)
+            self.b.neighbors.remove(self.a) # pretty ugly but ok
 
+        def init_neighbors(self):
+            self.a.neighbors.append(self.b)
         def __str__(self) -> str:
             return f"{self.a} --{self.cost}--> {self.b}"
         def __eq__(self, __o: object) -> bool:
@@ -68,7 +74,32 @@ class DirectedGraph(Graph):
             self.nodes.append(self.Node(i))
 
         for n in self.nodes:
-            e = self.DirectedEdge(n, self.nodes[random.randint(0, size -1)],random.randint(mincost, maxcost))
+            e = self.Edge(n, self.nodes[random.randint(0, size -1)],random.randint(mincost, maxcost))
             if e.a != e.b and e not in self.edges:
                 e.a.neighbors.append(e.b)
                 self.edges.append(e)
+
+class Tree(Graph):
+    pass # TODO
+
+def generate(size = 10, mincost = 1, maxcost = 100, type = "none"):
+    nodes = []
+    edges = []
+    for i in range(size):
+        nodes.append(Graph.Node(i))
+
+    for n in nodes:
+        if type == "none":
+            e = Graph.Edge(n, nodes[random.randint(0, size -1)],random.randint(mincost, maxcost))
+            if e.a != e.b and e not in edges:
+                e.a.neighbors.append(e.b)
+                e.b.neighbors.append(e.a)
+                edges.append(e)
+
+        elif type == "directed":
+            e = DirectedGraph.Edge(n, nodes[random.randint(0, size -1)],random.randint(mincost, maxcost))
+            if e.a != e.b and e not in edges:
+                e.a.neighbors.append(e.b)
+                edges.append(e)
+
+    return Graph(edges, nodes)
--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 
-from Graphs import Graph, DirectedGraph
+from Graphs import Graph
+import random
 
-g = DirectedGraph()
-g.generate()
-print(g)
+class Ant:
+
+    def chooseEdge():
+        pass
+
+# the  graph is fully connected so we assume we wont get stuck
+def get_random_loop_Path(start, graph):
+    visited = []
+    current = start
+    while len(visited) != graph.nodeCount():
+        c = current.getNeighbors()[random.randint(0, current.neighborsCount() -1)]
+        if c.id not in visited:
+            current = c
+            visited.append(c.id)
+    return visited
+
+# create a fully connected graph
+cities = [Graph.Node("Tel Aviv"), 
+Graph.Node("Kfar Saba"),
+Graph.Node("Eilat"),
+Graph.Node("Haifa"),
+Graph.Node("Beer Sheva")]
+
+roads = [Graph.Edge(cities[0], cities[1], 30),
+ Graph.Edge(cities[0], cities[2], 400),
+ Graph.Edge(cities[0], cities[3], 100),
+ Graph.Edge(cities[0], cities[4], 150),
+ Graph.Edge(cities[1], cities[2], 450),
+ Graph.Edge(cities[1], cities[3], 80),
+ Graph.Edge(cities[1], cities[4], 180),
+ Graph.Edge(cities[2], cities[4], 200),
+ Graph.Edge(cities[2], cities[3], 620),
+ Graph.Edge(cities[3], cities[4], 580)]
+
+
+israel = Graph(cities, roads)
+
+print(get_random_loop_Path(israel.getNodeById("Tel Aviv"), israel))
